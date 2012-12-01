a8fd4f6fd210e2583a71802a60af90b2a5b5cb3b
Andrey F. Mindubaev <covex.mobile@gmail.com>
2012-12-01 23:38:11 +0300

    Partition tree-changes

--- Wrapper/FS/Partition/Changes/JooS_Stream_Wrapper_FS_Partition_Changes_TreeTest.php
+++ Wrapper/FS/Partition/Changes/JooS_Stream_Wrapper_FS_Partition_Changes_TreeTest.php
@@ -9,12 +9,12 @@ class JooS_Stream_Wrapper_FS_Partition_Changes_TreeTest extends PHPUnit_Framewor
   {
     $changes = new JooS_Stream_Wrapper_FS_Partition_Changes_Tree();
 
-    $this->assertFalse($changes->exists("qqq/www"));
-    $this->assertEquals(null, $changes->get("qqq/www"));
-    $this->assertFalse($changes->delete("qqq/www"));
+    $this->assertFalse($changes->exists("qqq/www/eee"));
+    $this->assertEquals(null, $changes->get("qqq/www/eee"));
+    $this->assertFalse($changes->delete("qqq/www/eee"));
     $this->assertEquals(0, $changes->count());
     $this->assertEquals(array(), $changes->children());
-    
+
     require_once "JooS/Stream/Entity.php";
 
     $entity = JooS_Stream_Entity::newInstance(__FILE__);
@@ -23,20 +23,25 @@ class JooS_Stream_Wrapper_FS_Partition_Changes_TreeTest extends PHPUnit_Framewor
 
     $storage = new JooS_Stream_Storage($entity);
 
-    $changes->add("qqq/www", $storage);
+    $changes->add("qqq/www/eee", $storage);
 
-    $this->assertTrue($changes->exists("qqq/www"));
-    $this->assertEquals($storage, $changes->get("qqq/www"));
+    $this->assertTrue($changes->exists("qqq/www/eee"));
+    $this->assertEquals($storage, $changes->get("qqq/www/eee"));
     $this->assertEquals(1, $changes->count());
-    $this->assertEquals(array($storage), $changes->children());
-    $this->assertEquals(array($storage), $changes->children("qqq"));
-    $this->assertEquals(array(), $changes->children("qqq/www"));
+    
+    $expectedChildren = array(
+      "qqq/www/eee" => $storage
+    );
+    $this->assertEquals($expectedChildren, $changes->children());
+    $this->assertEquals($expectedChildren, $changes->children("qqq"));
+    $this->assertEquals($expectedChildren, $changes->children("qqq/www"));
+    $this->assertEquals(array(), $changes->children("qqq/www/eee"));
 
-    $this->assertTrue($changes->delete("qqq/www"));
+    $this->assertTrue($changes->delete("qqq/www/eee"));
 
-    $this->assertFalse($changes->exists("qqq/www"));
-    $this->assertEquals(null, $changes->get("qqq/www"));
-    $this->assertFalse($changes->delete("qqq/www"));
+    $this->assertFalse($changes->exists("qqq/www/eee"));
+    $this->assertEquals(null, $changes->get("qqq/www/eee"));
+    $this->assertFalse($changes->delete("qqq/www/eee"));
     $this->assertEquals(0, $changes->count());
     $this->assertEquals(array(), $changes->children());
   }
